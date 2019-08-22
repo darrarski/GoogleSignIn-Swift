4771554c795689cd81be159b39686c92b77867e7
Dariusz Rybicki <darrarski@gmail.com>
2019-08-22 14:29:20 +0200

    Add public init to Config struct

--- Sources/GoogleSignIn/Config.swift
+++ Sources/GoogleSignIn/Config.swift
@@ -1,5 +1,15 @@
 public struct Config {
+
+    public init(clientId: String,
+                clientSecret: String,
+                redirectUri: String) {
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.redirectUri = redirectUri
+    }
+
     public var clientId: String
     public var clientSecret: String
     public var redirectUri: String
+
 }
